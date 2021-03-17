bb66974efec8dd346876ac992d65aef0c1471fb4
Nathan Hentges <nathanjhentges@gmail.com>
2021-03-16 21:27:17 -0500

    completed js functionality

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 
 // Grab DOM elements                    ==================
 var startCard = document.getElementById("start");
-var gameoverCard = document.getElementById("gameover");
+var gameOverCard = document.getElementById("gameover");
 var scoreCard = document.getElementById("highscores");
 
 var questionCards = document.getElementById("question-container");
@@ -12,10 +12,23 @@ var backBtn = document.getElementById("hs-back");
 var clearBtn = document.getElementById("hs-clear");
 
 var viewHs = document.getElementById("view-hs");
+var initials = document.getElementById("name-hs");
+var hsList = document.getElementById("highscore-list");
+
+var timeLeft = document.getElementById("time-left");
+var mainEl = document.getElementById("main");
+var quizScore = document.getElementById("quiz-score");
 
 // Global variables
 
+const timeAllowed = 60;
+
+var gameRunning = false;
+
 var currentQuestion = 0;
+var currentScore = timeAllowed;
+
+var timer = null;
 
 // Bind click events                    ==================
 document.addEventListener("click", docClick);
@@ -38,38 +51,140 @@ function docClick(event) {
     case viewHs:
       viewHsClick();
       break;
-  
-    default:
-      console.log("nothing important");
+      
+      default:
+        // Check if button for question is pressed
+        if (event.target.classList.contains("question-button")) {
+          questionAnswered(event.target);
+        }
       break;
   }
 }
 
 // Functions for various button clicks  ==================
 function startClick() {
-  startCard.classList.add("hidden");
+  hideEl(startCard);
   startQuiz();
 }
 function submitClick() {
+  let scoreString = `Player ${initials.value} scored ${currentScore}`
+  localStorage.setItem(localStorage.length, scoreString);
 
+  hideEl(gameOverCard);
+  showEl(scoreCard);
+  showScores();
 }
 function backClick() {
-
+  hideEl(scoreCard);
+  showEl(startCard);
 }
 function clearClick() {
-
+  localStorage.clear();
+  console.log(localStorage);
+  showScores();
 }
 function viewHsClick() {
-
+  // check if game is running, ignore if it isf
+  if (!gameRunning) {
+    hideEl(startCard);
+    hideEl(gameOverCard);
+    showEl(scoreCard);
+    showScores();
+  }
 }
+
+// Side functions                       ==================
+
+function runTimer() {
+  // Initialize
+  currentScore = timeAllowed;
+  timeLeft.innerText = currentScore;
+
+  timer = setInterval(function() {
+    if (currentScore > 0) {
+      currentScore--;
+      timeLeft.innerText = currentScore;
+    } else {
+      // Time ran out
+      gameOver();
+    }
+  }, 100);
+}
+
+function showScores() {
+  hsList.textContent="";
+  for (let i = 0; i < localStorage.length; i++) {
+    let scoreUnit = document.createElement("div")
+    scoreUnit.setAttribute("class", "user-score");
+    if (i % 2 === 0) {
+      scoreUnit.setAttribute("class", "alt-user-score")
+    }
+    scoreUnit.innerText = `${i+1}. ${localStorage[i]}`
+    hsList.append(scoreUnit);
+  }
+}
+
+// Hide/show things
+function hideEl(elem) {
+  elem.classList.add("hidden");
+}
+function showEl(elem) {
+  elem.classList.remove("hidden");
+}
+
+// Handles what happens when question is answered
+function questionAnswered(choice) {
+  if (! choice.dataset.correct) {
+    mainEl.classList.add("wrong-answer");
+    currentScore -= 10;
+    if (currentScore <= 0) {
+      currentScore = 0;
+      timeLeft.innerText = currentScore;
+      gameOver();
+      return;
+    }
+    timeLeft.innerText = currentScore;
+  } else {
+    // Correct choice
+    mainEl.classList.add("right-answer");
+  }
+  setTimeout(function() {
+    mainEl.classList.remove("wrong-answer", "right-answer");
+  }, 300);
+  hideEl(questionCards.children[currentQuestion]);
+  if (currentQuestion >= questionCards.childElementCount - 1) {
+    // No more questions
+    gameOver();
+  } else {
+    currentQuestion++;
+    // Question answered, show next question card
+    showEl(questionCards.children[currentQuestion]);
+  }
+}
+
 
 
 // Main quiz logic                      ==================
 function startQuiz() {
+  gameRunning = true;
+  viewHs.classList.add("not-allowed");
+  runTimer();
+  currentQuestion = 0;
+  
+  // Show first question card to start the question progression
   let question = questionCards.children[currentQuestion];
-  question.classList.remove("hidden")
+  showEl(question);
+  
 }
 
+function gameOver() {
+  clearInterval(timer);
+  hideEl(questionCards.children[currentQuestion]);
+  showEl(gameOverCard);
+  quizScore.innerText = currentScore;
+  viewHs.classList.remove("not-allowed");
+  gameRunning = false;
+}
 
 
 
